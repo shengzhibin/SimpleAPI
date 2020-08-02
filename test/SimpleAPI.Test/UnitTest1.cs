c24a58bda281b490885692b9a77bb94733f699dd
shengzhibin <sheng.zhibin@outlook.com>
2020-08-02 19:20:37 -0400

    Initial commit

---
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+
+namespace SimpleAPI.Test
+{
+    public class UnitTest1
+    {
+        ValuesController controller = new ValuesController ();
+        [Fact]
+        public void GetReturnMyName()
+        {
+            var returnValue=controller.Get(1);
+            Assert.Equal("Zhibin",returnValue);
+        }
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
